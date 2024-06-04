fc5611fd97ac51e267ccf157ba7f276f20ba9b07
Gustavo <gus.atanazio@gmail.com>
2024-06-04 14:52:41 -0300

    Adicionando comentários

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 import streamlit as st
 
+# Cores de cada nível de alerta (em CSS)
 colors = {
     'high': 'radial-gradient(circle, rgba(176,10,10,1) 0%, rgba(2,0,36,1) 100%);',
     'medium': 'radial-gradient(circle, rgba(203,160,18,1) 0%, rgba(2,0,36,1) 100%);',
     'low': 'radial-gradient(circle, rgba(9,121,28,1) 0%, rgba(2,0,36,1) 100%);'
 }
 
+# Peso de cada variável
 pounds = {
-    'plastic': 0.5,
-    'oxygen': 0.3,
-    'oil': 0.7
+    'plastic': 0.65,
+    'oxygen': 0.45,
+    'oil': 0.85
 }
 
+# Função que realiza o cálculo final
 def calc_risk(plastic, oxygen, oil):
     risk = (
         plastic * pounds['plastic']
@@ -19,8 +22,11 @@ def calc_risk(plastic, oxygen, oil):
         + oil * pounds['oil']
     )
 
+    # O risco total é calculado pela soma de todas a variáveis com seus pesos
+
     return risk
 
+# Função responsável por mudar a cor de fundo da interface
 def change_color(color):
     st.markdown(f'''
         <style>
@@ -29,36 +35,61 @@ def change_color(color):
     ''', unsafe_allow_html = True)
 
 def main():
-    # Interface
+    # INTERFACE
     st.title('Simulador de impacto ambiental')
+    st.write('Para uma melhor visualização, habilite a opção de tela cheia.')
     st.divider()
 
-    col1, col2 = st.columns(spec = [0.4, 0.6], gap = 'large')
+    # Dividindo o layout em duas colunas
+    col1, col2 = st.columns(
+        spec = [0.4, 0.6], # Retorna duas colunas, uma com 40% de preenchimento, e outra com 60%
+        gap = 'large' # Espaço entre as colunas
+    )
 
-    ## Variáveis
+    ## VARIÁVEIS
     with col1:
         st.header('Variáveis')
 
-        plastic = st.slider('Nível de plástico', 0, 100, 0)
-        oxygen = st.slider('Nível de oxigênio', 0, 100, 0)
-        oil = st.slider('Nível de petróleo', 0, 100, 0)
+        plastic = st.slider(
+            'Nível de plástico', # Label
+            0, # Mínimo
+            100, # Máximo
+            0, # Valor inicial
+            help = '' # Texto de apoio
+        )
+
+        oxygen = st.slider(
+            'Nível de oxigênio',
+            0,
+            100,
+            0,
+            help = ''
+        )
+
+        oil = st.slider(
+            'Nível de petróleo',
+            0,
+            100,
+            0,
+            help = ''
+        )
 
     data = {
-        'Valores': [plastic, oxygen, oil],
-        'Nomes': ['Plástico', 'Oxigênio', 'Petróleo']
+        'Valores': [plastic, oxygen, oil], # Lista de valores
+        'Nomes': ['Plástico', 'Oxigênio', 'Petróleo'] # Labels
     }
 
-    ## Gráfico
+    ## GRÁFICO
     with col2:
         st.header('Gráfico')
 
         st.bar_chart(
-            data, 
-            x = 'Nomes', 
-            color = '#0000FF'
+            data, # Valores
+            x = 'Nomes', # Posição no dicionário contendo as labels
+            color = '#0000FF' # Cor das barras
         )
         
-    # Lógica
+    # LÓGICA
     total_risk = calc_risk(plastic, oxygen, oil)
 
     if total_risk > 70:
